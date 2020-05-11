d8d33cd8fa8235f144557b511d14a20d0c9ab480
Juliano Resende <juliano.resende@gmail.com>
2020-05-10 23:52:16 -0300

    ultima aula 10-05-2020

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask import request
+from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = 'juliano'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # creates /auth endpoint
+
+items = []
+
+class Item(Resource):
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items),None)
+        return {'item': item}, 200 if item else 404
+    
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items),None):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(debug=True)
